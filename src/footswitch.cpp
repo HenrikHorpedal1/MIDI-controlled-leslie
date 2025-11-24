82562e581ac70843640cf81a886af0274628ec6b
HenrikHorpedal1 <hhorpedal@gmail.com>
2025-11-24 14:10:48 +0100

    feat: refactor footswitch module for more readable code

--- src/footswitch.cpp
+++ src/footswitch.cpp
@@ -5,57 +5,93 @@
 #define PIN_B 12
 #define PIN_C 14
 
+const TickType_t LED_ON_TIME  = 1;
+const TickType_t LED_OFF_TIME = 9;
+
 volatile bool switchA = false;
 volatile bool switchB = false;
 
-void footSwitchTask(void *pvParameters) {
-  const TickType_t ledOnTime  = 1;
-  const TickType_t ledOffTime = 9;
-
-  for (;;) {
+static bool readSwitchA() {
     pinMode(PIN_B, OUTPUT);
     digitalWrite(PIN_B, LOW);
     pinMode(PIN_A, INPUT_PULLUP);
     pinMode(PIN_C, INPUT);
-    switchA = (digitalRead(PIN_A) == LOW);
 
+    return (digitalRead(PIN_A) == LOW);
+}
+
+static bool readSwitchB(){
     pinMode(PIN_C, OUTPUT);
     digitalWrite(PIN_C, LOW);
     pinMode(PIN_B, INPUT_PULLUP);
     pinMode(PIN_A, INPUT);
-    switchB = (digitalRead(PIN_B) == LOW);
+
+    return (digitalRead(PIN_B) == LOW); 
+}
+
+static void lightLedA(){
+    pinMode(PIN_A, OUTPUT);
+    pinMode(PIN_C, OUTPUT);
+    pinMode(PIN_B, INPUT);
+
+    digitalWrite(PIN_A, HIGH);
+    digitalWrite(PIN_C, LOW);
+
+    vTaskDelay(LED_ON_TIME);
+
+    //reset
+    pinMode(PIN_A, INPUT);
+    pinMode(PIN_C, INPUT);
+}
+
+static void lightLedB1(){
+    pinMode(PIN_B, OUTPUT);
+    pinMode(PIN_A, OUTPUT);
+    pinMode(PIN_C, INPUT);
+
+    digitalWrite(PIN_B, HIGH);
+    digitalWrite(PIN_A, LOW);
+
+    vTaskDelay(LED_ON_TIME);
+
+    //reset
+    pinMode(PIN_B, INPUT);
+    pinMode(PIN_A, INPUT);
+}
+
+static void lightLedB2(){
+    pinMode(PIN_C, OUTPUT);
+    pinMode(PIN_B, OUTPUT);
+    pinMode(PIN_A, INPUT);
+
+    digitalWrite(PIN_C, HIGH);
+    digitalWrite(PIN_B, LOW);
+
+    vTaskDelay(LED_ON_TIME);
+
+    //reset
+    pinMode(PIN_C, INPUT);
+    pinMode(PIN_B, INPUT);
+}
+
+void footSwitchTask(void *pvParameters) {
+
+  for (;;) {
+    switchA = readSwitchA();
+    switchB = readSwitchB();
 
     if (switchA) {
-      pinMode(PIN_A, OUTPUT);
-      pinMode(PIN_C, OUTPUT);
-      pinMode(PIN_B, INPUT);
-      digitalWrite(PIN_A, HIGH);
-      digitalWrite(PIN_C, LOW);
-      vTaskDelay(ledOnTime);
-      pinMode(PIN_A, INPUT);
-      pinMode(PIN_C, INPUT);
-    } else vTaskDelay(ledOnTime);
+        lightLedA();
+    } else{
+        vTaskDelay(LED_ON_TIME);
+    }
 
     if (switchB) {
-      pinMode(PIN_B, OUTPUT);
-      pinMode(PIN_A, OUTPUT);
-      pinMode(PIN_C, INPUT);
-      digitalWrite(PIN_B, HIGH);
-      digitalWrite(PIN_A, LOW);
-      vTaskDelay(ledOnTime);
-      pinMode(PIN_B, INPUT);
-      pinMode(PIN_A, INPUT);
+        lightLedB1();
     } else {
-      pinMode(PIN_C, OUTPUT);
-      pinMode(PIN_B, OUTPUT);
-      pinMode(PIN_A, INPUT);
-      digitalWrite(PIN_C, HIGH);
-      digitalWrite(PIN_B, LOW);
-      vTaskDelay(ledOnTime);
-      pinMode(PIN_C, INPUT);
-      pinMode(PIN_B, INPUT);
+        lightLedB2();
     }
-    vTaskDelay(ledOffTime);
+    vTaskDelay(LED_OFF_TIME);
   }
 }
 
--- src/footswitch.h
+++ src/footswitch.h
@@ -1,14 +1,3 @@
-#ifndef FOOTSWITCH_H
-#define FOOTSWITCH_H
-
-#ifdef __cplusplus
-extern "C" {            // prevents C++ name mangling when included from .ino/.cpp
-#endif
+#pragma once
 
 void footSwitchTask(void *pvParameters);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
-
-#endif // FOOTSWITCH_H
